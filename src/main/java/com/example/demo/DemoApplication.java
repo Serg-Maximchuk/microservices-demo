abe29e1b83ed2ba729e8b528036b8e6ac7036a98
Serhii Maksymchuk <serg.maximchuk@gmail.com>
2018-06-09 21:49:05 +0300

    Removed no more used files

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -3,13 +3,11 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.config.server.EnableConfigServer;
 
-@EnableConfigServer
 @SpringBootApplication
-public class ConfigServer {
+public class DemoApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(ConfigServer.class, args);
+        SpringApplication.run(DemoApplication.class, args);
     }
 }
